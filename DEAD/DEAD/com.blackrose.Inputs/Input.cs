7ac0c411900242f00e1ce015a619af687dcac3b2
Jonathan Coty <Jonathan.Coty@sgs.com>
2013-09-20 14:29:13 -0400

    20-Sep-2013 Update 1 (Cont)
    -didn't add the other two classes

---
+++ DEAD/DEAD/com.blackrose.Inputs/Input.cs
@@ -0,0 +1,47 @@
+﻿// Jonathan Coty
+// 19-Sep-2013
+// Black Rose Funeral Home
+
+// Controls the keyboard inputs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using DEAD.com.blackrose.Screens; 
+
+namespace DEAD.com.blackrose.Inputs
+{
+    class Input
+    {
+        private bool isKeyDown; 
+        
+        public Input() { 
+            isKeyDown = false; 
+        }
+
+        public void keyPress(Screen screen)
+        {
+            screen.input(Keyboard.GetState(), isKeyDown);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter) || Keyboard.GetState().IsKeyDown(Keys.Up) || Keyboard.GetState().IsKeyDown(Keys.Down) ||
+                Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.S))
+                isKeyDown = true;
+            else
+                isKeyDown = false;
+        }
+
+        public void mouseClick()
+        {
+
+        }
+
+        public bool isKeyPressed()
+        {
+            return isKeyDown; 
+        }
+    }
+}
---
+++ DEAD/DEAD/com.blackrose.UI/Footer.cs
@@ -0,0 +1,17 @@
+﻿// Jonathan Coty
+// 19-Sep-2013
+// Black Rose Funeral Home
+
+// Footer for player lives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEAD.com.blackrose.UI
+{
+    class Footer
+    {
+    }
+}
